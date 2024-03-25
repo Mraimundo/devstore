3544e01f456edc33e24633f82eb648fa46f321cb
Mraimundo <mdumundo18@gmail.com>
2024-03-25 16:49:32 -0300

    feat: add component header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html className={`antialiased ${inter.variable}`} lang="pt">
-      <body>{children}</body>
+    <html className={inter.variable} lang="pt">
+      <body className="antialiased bg-zinc-950 text-zinc-100">{children}</body>
     </html>
   );
 }
